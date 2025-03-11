742136fb9f43aa290d59ab6a8cd3b6af8802f051
Yashraj Nayak <yashrajnayak@github.com>
2025-03-11 22:44:46 +0530

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,10 @@ export default defineConfig({
   base: '/most-active-developers-india/',
   build: {
     outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
